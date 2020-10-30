4ef3b1e02ba8eb35a0787b9ff05f2c927b665cbb
wangzt568 <67421172+wangzt568@users.noreply.github.com>
2020-10-30 10:15:22 -0400

    Update and rename IngredientClientApplicationTests.java to BookingClientApplicationTests.java

--- booking-client/src/test/java/tacos/ingredientclient/BookingClientApplicationTests.java
+++ booking-client/src/test/java/tacos/ingredientclient/BookingClientApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IngredientClientApplicationTests {
+public class BookingClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
